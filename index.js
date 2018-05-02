06c58a593050e4e6869577be792bcf27ad0a436a
Rick Sun <yatyricky@gmail.com>
2018-05-02 14:27:09 +0800

    - 编辑图片标签 - 新增eslint

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-const config = require('./config');
-const fs = require('fs');
-const archiver = require('archiver');
-const https = require('https');
-const path = require('path');
-const express = require('express');
+const config = require("./config");
+const fs = require("fs");
+const archiver = require("archiver");
+const https = require("https");
+const path = require("path");
+const express = require("express");
 const app = express();
+const MongoClient = require("mongodb").MongoClient;
 
 https.createServer({
-    key: fs.readFileSync('/etc/letsencrypt/live/g.nefti.me/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/g.nefti.me/fullchain.pem')
+    key: fs.readFileSync("/etc/letsencrypt/live/g.nefti.me/privkey.pem"),
+    cert: fs.readFileSync("/etc/letsencrypt/live/g.nefti.me/fullchain.pem")
 }, app).listen(config.PORT, () => {
     console.log(`--- server running on ${config.PORT} ---`);
 });
 
 const files = [];
-fs.readdirSync('./files').map(file => {
-    files.push(file.split('.')[0]);
+fs.readdirSync("./files").map(file => {
+    files.push(file.split(".")[0]);
 });
 
 app.use(express.static(path.join(__dirname, "/public")));
@@ -24,40 +25,40 @@ app.use((req, res, next) => {
         if (req.query["secret"] == config.SECRET) {
             next();
         } else {
-            console.log('wrong secret');
-            res.status(404).send('Not found');
+            console.log("wrong secret");
+            res.status(404).send("Not found");
         }
     } else {
-        console.log('no secret');
-        res.status(404).send('Not found');
+        console.log("no secret");
+        res.status(404).send("Not found");
     }
 });
 
 app.get("/", (req, res) => {
     if (req.query.hasOwnProperty("list")) {
-        const fos = fs.createWriteStream('bulk.zip');
-        const archive = archiver('zip');
+        const fos = fs.createWriteStream("bulk.zip");
+        const archive = archiver("zip");
 
-        fos.on('close', () => {
-            console.log(archive.pointer() + ' total bytes');
-            console.log('archiver has been finalized and the output file descriptor has closed.');
-            res.status(200).sendFile(path.join(__dirname, 'bulk.zip'));
+        fos.on("close", () => {
+            console.log(archive.pointer() + " total bytes");
+            console.log("archiver has been finalized and the output file descriptor has closed.");
+            res.status(200).sendFile(path.join(__dirname, "bulk.zip"));
         });
-        
-        archive.on('error', (err) => {
+
+        archive.on("error", (err) => {
             console.log(err);
-            res.status(201).send('failed');
+            res.status(201).send("failed");
         });
-        
+
         archive.pipe(fos);
         const has = req.query["list"].split(",");
         console.log(`Request List = ${req.query["list"]}`);
-        
+
         let count = 0;
         for (let i = 0; i < files.length; i++) {
             if (has.indexOf(files[i]) == -1 && count < config.MAX_FILES) {
-                fn = files[i] + ".json";
-                archive.file(path.join('files', fn), {
+                const fn = files[i] + ".json";
+                archive.file(path.join("files", fn), {
                     name: fn
                 });
                 count += 1;
@@ -65,13 +66,78 @@ app.get("/", (req, res) => {
         }
         archive.finalize();
     } else {
-        console.log('no list');
-        res.status(400).send('bad request');
+        console.log("no list");
+        res.status(400).send("bad request");
     }
 });
 
 app.get("/vortex", (req, res) => {
     res.status(200).sendFile(path.join(__dirname, "/web/index.html"));
+});
+
+app.get("/list", (req, res) => {
+    res.status(200).send(files);
+});
+
+app.get("/gettag", (req, res) => {
+    if (req.query.hasOwnProperty("name")) {
+        MongoClient.connect("mongodb://localhost:27017/", (err, db) => {
+            if (err) {
+                res.status(500).send(`Internal error ${JSON.stringify(err)}`);
+            } else {
+                db.db("pixels").collection("filedata").find({
+                    name: req.query.name
+                }).toArray((err, dbres) => {
+                    if (err) {
+                        res.status(500).send(`Internal error ${JSON.stringify(err)}`);
+                    } else {
+                        if (dbres.length == 1) {
+                            res.status(200).send(dbres[0].tags);
+                        } else if (dbres.length == 0) {
+                            res.status(200).send([]);
+                        } else {
+                            res.status(201).send(`Warning: duplicated ${req.query.name}`);
+                        }
+                        db.close();
+                    }
+                });
+            }
+        });
+    } else {
+        res.status(400).send("Bad request: no name received");
+    }
+});
+
+app.get("/update", (req, res) => {
+    if (req.query.hasOwnProperty("name") && req.query.hasOwnProperty("tags")) {
+        MongoClient.connect("mongodb://localhost:27017/", (err, db) => {
+            if (err) {
+                res.status(500).send(`Internal error ${JSON.stringify(err)}`);
+            } else {
+                db.db("pixels").collection("filedata").updateOne(
+                    {
+                        name: req.query.name
+                    }, {
+                        $set: {
+                            name: req.query.name,
+                            tags: req.query.tags.split(",")
+                        }
+                    }, {
+                        upsert: true
+                    }, (err, dbres) => {
+                        if (err) {
+                            res.status(500).send(`Internal error ${JSON.stringify(err)}`);
+                        } else {
+                            db.close();
+                            res.status(200).send("Upsert OK");
+                        }
+                    }
+                );
+            }
+        });
+    } else {
+        res.status(400).send("Bad request: no name and tags received");
+    }
 });
 
 app.get("*", (req, res) => {
--- web/Main.jsx
+++ web/Main.jsx
@@ -1,12 +1,140 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
+import axios from "axios";
 
-class Main extends React.Component {
-    render () {
+class PixelItem extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+        this.validateInput = this.validateInput.bind(this);
+        this.requestTag = this.requestTag.bind(this);
+        this.state = {
+            tagsValue: ""
+        };
+        setTimeout(() => {
+            this.requestTag();
+        }, 0);
+    }
+
+    requestTag() {
+        const secret = new URL(window.location.href).searchParams.get("secret");
+        axios({
+            url: `/gettag?name=${this.props.name}&secret=${secret}`,
+            method: "get",
+        }).then((response) => {
+            this.setState({
+                tagsValue: response.data.join(",")
+            });
+        }).catch((error) => {
+            this.appendMessage(error.response);
+        });
+    }
+
+    validateInput() {
+        this.setState({
+            tagsValue: this.refs.inputTags.value
+        });
+    }
+
+    handleClick() {
+        this.props.setTags(this.props.name, this.state.tagsValue);
+    }
+
+    render() {
         return (
-            <div>OJBK</div>
+            <tr>
+                <td><img src={`/${this.props.name}.png`} /></td>
+                <td>{this.props.name}</td>
+                <td><input type="text" ref="inputTags" onChange={this.validateInput} value={this.state.tagsValue} /></td>
+                <td><button type="button" onClick={this.handleClick}>Commit</button></td>
+            </tr>
         );
     }
 }
 
-render(<Main />, document.getElementById('root'));
+class Main extends React.Component {
+
+    constructor() {
+        super();
+
+        this.requestList = this.requestList.bind(this);
+        this.setTags = this.setTags.bind(this);
+
+        this.state = {
+            entries: [],
+            messages: [(<div key="0">Messages:</div>)]
+        };
+
+        this.requestList();
+    }
+
+    appendMessage(msg) {
+        const newarr = this.state.messages;
+        newarr.push(
+            <div key={this.state.messages.length}>{JSON.stringify(msg)}</div>
+        );
+        this.setState({
+            messages: newarr
+        });
+    }
+
+    setTags(name, tags) {
+        const secret = new URL(window.location.href).searchParams.get("secret");
+        axios({
+            url: `/update?name=${name}&tags=${tags}&secret=${secret}`,
+            method: "get",
+        }).then((response) => {
+            this.appendMessage(response.data);
+        }).catch((error) => {
+            this.appendMessage(error.response);
+        });
+    }
+
+    requestList() {
+        const secret = new URL(window.location.href).searchParams.get("secret");
+        const axiosConfig = {
+            url: `/list?secret=${secret}`,
+            method: "get",
+        };
+
+        axios(axiosConfig).then((response) => {
+            const arr = [];
+            for (let i = 0; i < response.data.length; i++) {
+                const element = response.data[i];
+                arr.push(
+                    <PixelItem key={i} name={element} setTags={this.setTags} />
+                );
+                this.setState({
+                    entries: arr
+                });
+            }
+        }).catch((error) => {
+            this.appendMessage(error.response);
+        });
+    }
+
+    render () {
+        return (
+            <div>
+                <div className="resultPane">{this.state.messages}</div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Tags</th>
+                            <th>Commit</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.entries}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+
+}
+
+render(<Main />, document.getElementById("root"));
